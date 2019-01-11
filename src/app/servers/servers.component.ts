9c4ea5aa3d5cbe8eda2e805ef914aba90a11fdc0
yuryklim <yuryklim@outlook.com>
2019-01-11 23:14:41 +0200

    adding a new property to class ServersComponent

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
 
   constructor() { }
 
