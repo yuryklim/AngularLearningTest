59e9fd7f9263e4f393ef33c6fe17587b3bdd23fb
yuryklim <yuryklim@outlook.com>
2019-01-11 23:22:34 +0200

    perform setTimeout() JS function in constructor of ServersComponent class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    }, 2000)
+   }
 
   ngOnInit() {
   }
